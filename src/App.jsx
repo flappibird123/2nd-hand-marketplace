68689f4c44a3dbf5520fa3e0bdbc3efe260cac68
flappibird123 <jasonlilb2@gmail.com>
2025-11-17 00:53:24 +0000

    finished Title component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Title } from './components/Title/Title.jsx'
 function App() {
   return (
     <>
-      <Title title="Chair" />
+      <Title title=""/>
     </>
   )
 }
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,5 +1,7 @@
 import styles from './Title.module.css'
 
-export function Title({ title }) {
-    return(<h1 className={styles.h1}>{title}</h1>);
+export function Title({ title = "Untitled" }) {
+    const displayTitle = title.length === 0 ? "Untitled" : title;
+
+    return(<h1 className={styles.h1}>{displayTitle}</h1>);
 }
